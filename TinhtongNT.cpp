bf80a53434a975132b85d829e29c4ba7343f4566
Duongthanh <haduongthanh12345678@gmail.com>
2022-10-27 16:07:56 +0700

    Tong NT

---
+++ TinhtongNT.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream> 
+using namespace std;
+int main ()
+{
+	int n;
+	cout<<"Nhap n: ";
+	if (n<0 or n>20)
+	cout <<"Nhap lai n=";
+	else if (0<n<20)
+	cin>> n;
+	return 0;
+}
+
