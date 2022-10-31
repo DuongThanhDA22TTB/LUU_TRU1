44a116beccb6b2d1d366f4daa839a7381e60dba2
DuongThanhDA22TTB <116790527+DuongThanhDA22TTB@users.noreply.github.com>
2022-10-31 11:39:53 +0700

    Update TinhtongNT.cpp

--- TinhtongNT.cpp
+++ TinhtongNT.cpp
@@ -3,12 +3,32 @@
 using namespace std;
 int main ()
 {
-	int n;
-	cout<<"Nhap n: ";
-	if (n<0 or n>20)
-	cout <<"Nhap lai n=";
-	else if (0<n<20)
-	cin>> n;
+    long temp, n;
+  int TONGNT = 1;
+  KTNT(int x);
+do
+  {
+    printf("\nNhap vào so nguyên n lon hon 0 va nho hon 20: ");
+    scanf("%ld", &n);
+    if(n <= 0 and n >=20)
+    {
+      printf("\n So n phai lon hon 0 va nho hon 20, vui lòng nhap lai !");
+    }
+  }while(n <= 0 and n >=20);
 	return 0;
+	if(KTSNT(n)==true)
+		cout<< n << " la so nguyen to!";
+	else
+		cout<< n <<" khong la so nguyen to!";
+	cout<<endl;
+	return 0;
+	temp = n;
+  while(temp != 0)
+  {
+     P = P + (temp % 10);
+     temp = temp / 10;
+  }
+  cout<<"\nTong cua cac so nguyen to "<<n<<" la: "<< P;
+  return 0;
+	
 }
-
